51d337d59fafe87875a9d1022df3fff516fff258
Thiago Ribeiro <thiago.escola321@gmail.com>
2021-03-03 17:13:03 -0300

    Primeira Versão do Programa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word!");
+        }
+    }
+}
